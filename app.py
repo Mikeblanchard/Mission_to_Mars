aef24a380b16757ad00f9d3701584bda5bdba7db
Mikeblanchard <76125201+Mikeblanchard@users.noreply.github.com>
2021-03-07 23:35:07 -0500

    working app upload.

--- app.py
+++ app.py
@@ -1,3 +1,63 @@
-from flask import Flask, render_template
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import Mission_to_Mars
+import scrapping
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+
+# In[4]:
+
+
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+
+# In[5]:
+
+
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scrapping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return redirect('/', code=302)
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+   app.run()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
